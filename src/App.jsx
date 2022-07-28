5364988ad92ab9a5e4bdd3462e74d70aecde1ca6
ZakariaHn <zakaria@posteo.net>
2022-07-28 11:53:02 +0200

    suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
-import Books from './books.json'
-import './App.css';
+import Books from "./books.json";
+import Card from "./components/Card";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Bestseller Books</header>
       <div className="container">
-      {Books && Books.map( books =>{
-        return  (        
-          <div className="box" key={books.id}>
-           <img src={books.image} alt="books"/>
-           <div className="card">
-            <h2>{books.title}</h2>
-            <h4>{books.author}</h4>
-            <div className="details">{books.details && books.details.map(data => {
-              return(
-                <div key={books.id}>
-                  <p>{data.genre}</p>
-                  <p>{data.year}</p>
-                  </div>              
-              )
-            })}</div>
-            </div>
-            </div>    
-    )
-    })
-    }
-    </div>
+        {Books &&
+          Books.map((books) => {
+            return (
+              <div className="box" key={books.id}>
+                <Card books={books} />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
