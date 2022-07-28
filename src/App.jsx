55973a1ab9fe0793ae949b3664c5d913f7f4089a
NargizaRA <rysmendieva.nargiza@gmail.com>
2022-07-28 09:49:03 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from "react"
+import Books from './books.json'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>My React App</h1>
+      <header className="App-header">Bestseller Books</header>
+      <div className="container">
+      {Books && Books.map( books =>{
+        return  (        
+          <div className="box" key={books.id}>
+           <img src={books.image} alt="books"/>
+           <div className="card">
+            <h2>{books.title}</h2>
+            <h4>{books.author}</h4>
+            <div className="details">{books.details && books.details.map(data => {
+              return(
+                <div key={books.id}>
+                  <p>{data.genre}</p>
+                  <p>{data.year}</p>
+                  </div>              
+              )
+            })}</div>
+            </div>
+            </div>    
+    )
+    })
+    }
+    </div>
     </div>
   );
 }
