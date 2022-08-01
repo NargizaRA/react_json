ca607042173e382ba0a5b6f89bd516e5dff81a30
NargizaRA <rysmendieva.nargiza@gmail.com>
2022-08-01 09:42:49 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import Books from "./books.json";
-import Card from "./components/Card";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Bestseller Books</header>
-      <div className="container">
-        {Books &&
-          Books.map((books) => {
-            return (
-              <div className="box" key={books.id}>
-                <Card books={books} />
-              </div>
-            );
-          })}
-      </div>
-    </div>
-  );
-}
+//create your function here
 
 export default App;
